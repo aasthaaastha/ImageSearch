ac0cd2e171837ffca426acf92317cca5eb98db08
Aastha <aastha847@gmail.com>
2023-01-08 22:11:45 +0530

    Updated the photos model and the categories model

--- lib/bindable/adaptive.dart
+++ lib/bindable/adaptive.dart
@@ -1,29 +1,12 @@
-// Copyright (c) 2022 yichen <d.unicreators@gmail.com>. All rights reserved.
-// Use of this source code is governed by a MIT license that can be
-// found in the LICENSE file.
+
 
 part of '../mvvm.dart';
 
 ///
-/// 适配绑定属性
-///
 class AdaptiveBindableProperty<TValue, TAdaptee extends Listenable>
     extends CustomBindableProperty<TValue> {
-  /// 被适配对象
-  final TAdaptee adaptee;
 
-  ///
-  /// 创建一个适配绑定属性
-  ///
-  /// [adaptee] 被适配者实例，适配者必须继承自 [Listenable]
-  ///
-  /// [valueGetter] 指定从被适配者获取值的方法
-  ///
-  /// [valueSetter] 指定设置被适配者值的方法
-  ///
-  /// [valueChanged] 指定属性值变更后的回调方法
-  ///
-  ///
+  final TAdaptee adaptee;
   AdaptiveBindableProperty(this.adaptee,
       {required TValue Function(TAdaptee) valueGetter,
       void Function(TAdaptee, TValue)? valueSetter,
--- lib/bindable/async.dart
+++ lib/bindable/async.dart
@@ -1,12 +1,7 @@
-// Copyright (c) 2022 yichen <d.unicreators@gmail.com>. All rights reserved.
-// Use of this source code is governed by a MIT license that can be
-// found in the LICENSE file.
+
 
 part of '../mvvm.dart';
 
-///
-/// 异步请求绑定属性
-///
 class AsyncBindableProperty<TValue>
     extends BindableProperty<AsyncSnapshot<TValue>> {
   Object? _callbackIdentity;
@@ -18,26 +13,6 @@ class AsyncBindableProperty<TValue>
   final void Function()? _onEnd;
   final void Function(TValue)? _onSuccess;
   final void Function(dynamic)? _onError;
-
-  ///
-  /// 具备处理异步请求的绑定属性
-  ///
-  /// [futureGetter] 用于获取 [Future<TValue>] 的方法
-  ///
-  /// [handle] 指定请求成功时对结果进行处理的方法
-  ///
-  /// [onStart] 指定请求发起时执行的方法
-  ///
-  /// [onEnd] 指定请求结束时执行的方法
-  ///
-  /// [onSuccess] 指定请求成功时执行的方法
-  ///
-  /// [onError] 指定请求出错时执行的方法
-  ///
-  /// [valueChanged] 指定属性值变更后的回调方法
-  ///
-  /// [initial] 指定初始值
-  ///
   AsyncBindableProperty(AsyncValueGetter<TValue> futureGetter,
       {TValue Function(TValue)? handle,
       void Function()? onStart,
@@ -68,9 +43,6 @@ class AsyncBindableProperty<TValue>
     }
   }
 
-  ///
-  /// 发起请求
-  ///
   @protected
   void invoke({bool resetOnBefore = true}) {
     if (resetOnBefore) {
@@ -99,20 +71,8 @@ class AsyncBindableProperty<TValue>
   }
 }
 
-///
-/// 异步请求绑定属性
-///
 mixin AsyncBindablePropertyMixin on BindableObjectMixin {
-  ///
-  /// 获取指定 [propertyKey] 对应异步请求属性的请求发起方法
-  ///
-  /// [propertyKey] 对应属性必须为 [AsyncBindableProperty] 否则返回 `null`
-  ///
-  /// [resetOnBefore] 指定发起请求之前是否重置属性值
-  ///   当其值为 `true` 时, 发起请求之前属性值将先被重置为 `AsyncSnapshot<TValue>.nothing()`
-  ///
-  /// 调用其返回的方法将发起异步请求
-  ///
+
   void Function()? getInvoke(Object propertyKey, {bool resetOnBefore = true}) {
     var property = getPropertyOf<dynamic, AsyncBindableProperty>(propertyKey);
     return property != null
@@ -120,42 +80,15 @@ mixin AsyncBindablePropertyMixin on BindableObjectMixin {
         : null;
   }
 
-  ///
-  /// 获取指定 [propertyKey] 对应异步请求属性的请求发起方法
-  ///
-  /// [propertyKey] 对应属性必须为 [AsyncBindableProperty]
-  ///
-  /// [resetOnBefore] 指定发起请求之前是否重置属性值
-  ///   当其值为 `true` 时, 发起请求之前属性值将先被重置为 `AsyncSnapshot<TValue>.nothing()`
-  ///
-  /// 调用其返回的方法将发起异步请求
-  ///
   void Function() requireInvoke(Object propertyKey,
       {bool resetOnBefore = true}) {
     return getInvoke(propertyKey, resetOnBefore: resetOnBefore)!;
   }
 
-  ///
-  /// 发起指定 [propertyKey] 对应的异步请求
-  ///
-  /// [propertyKey] 对应属性必须为 [AsyncBindableProperty] 否则空操作
-  ///
-  /// [resetOnBefore] 指定发起请求之前是否重置属性值
-  ///   当其值为 `true` 时, 发起请求之前属性值将先被重置为 `AsyncSnapshot<TValue>.nothing()`
-  ///
-  /// 调用其返回的方法将发起异步请求
-  ///
+
   void invoke(Object propertyKey, {bool resetOnBefore = true}) =>
       getInvoke(propertyKey, resetOnBefore: resetOnBefore)?.call();
 
-  ///
-  /// 获取指定 [propertyKey] 对应异步请求发起链接
-  ///
-  /// [resetOnBefore] 指定发起请求之前是否重置属性值
-  ///   当其值为 `true` 时, 发起请求之前属性值将先被重置为 `AsyncSnapshot<TValue>.nothing()`
-  ///
-  /// 其实质为 [getInvoke] 的快捷操作
-  ///
   void Function()? link(Object propertyKey, {bool resetOnBefore = true}) =>
       getInvoke(propertyKey, resetOnBefore: resetOnBefore);
 }
