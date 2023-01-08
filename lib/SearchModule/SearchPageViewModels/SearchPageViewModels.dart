6e452603d3c9c2cfc5c6def42b373755f9f6e2dd
Aastha <aastha847@gmail.com>
2023-01-08 18:02:00 +0530

    updated the implementation

--- lib/SearchModule/SearchPageViewModels/SearchPageViewModels.dart
+++ lib/SearchModule/SearchPageViewModels/SearchPageViewModels.dart
@@ -1,79 +1,20 @@
-// Copyright (c) 2022 yichen <d.unicreators@gmail.com>. All rights reserved.
-// Use of this source code is governed by a MIT license that can be
-// found in the LICENSE file.
+class Images {
+  String title, openingCrawl, director, producer;
+  DateTime releaseDate;
 
-part of './mvvm.dart';
+  Images({
+    this.title,
+    this.openingCrawl,
+    this.director,
+    this.producer,
+    this.releaseDate,
+  });
 
-/// ViewModel
-///
-abstract class ViewModel extends ViewModelBase
-    with
-        BindableObjectMixin,
-        BindableObjectValueMixin,
-        ValueWidgetBuilderMixin,
-        BindableObjectWidgetBuilderMixin,
-        AsyncBindablePropertyMixin {}
-
-/// ViewModelBase
-///
-abstract class ViewModelBase implements BindableObject {
-  Map<Object, BindableProperty<dynamic>>? _properties;
-
-  ///
-  /// 获取所有已注册的属性
-  @visibleForTesting
-  @protected
-  Iterable<MapEntry<Object, BindableProperty<dynamic>>> get properties =>
-      _properties?.entries ?? [];
-
-  ///
-  /// 注册一个绑定属性
-  ///
-  /// [propertyKey] 指定属性键
-  ///
-  /// [property] 指定绑定属性
-  ///
-  @override
-  BindableProperty<TValue> registerProperty<TValue>(
-      Object propertyKey, BindableProperty<TValue> property) {
-    (_properties ??= <Object, BindableProperty<dynamic>>{})[propertyKey] =
-        property;
-    return property;
-  }
-
-  ///
-  /// 获取指定 [propertyKey] 对应的属性
-  ///
-  /// [propertyKey] 属性键
-  ///
-  /// [required] 指定 [propertyKey] 对应属性是否必须存在,
-  ///   其值为 `true` 时, 如 [propertyKey] 对应属性不存在则抛出异常
-  ///   默认值为 `false`
-  ///
-  @override
-  BindableProperty<TValue>? getProperty<TValue>(Object propertyKey,
-      {bool required = false}) {
-    var property = _properties?[propertyKey] as BindableProperty<TValue>?;
-    if (required && property == null)
-      throw NotfoundPropertyException(propertyKey);
-    return property;
-  }
-
-  ///
-  @protected
-  @mustCallSuper
-  void init() {}
-
-  /// dispose
-  @protected
-  @mustCallSuper
-  void dispose() {
-    if (_properties == null) return;
-    if (!_properties!.isNotEmpty) {
-      for (var prop in _properties!.values) {
-        prop.dispose();
-      }
-    }
-    _properties = null;
+  Images.fromMap(Map<String, dynamic> map) {
+    title = map['title'];
+    openingCrawl = map['opening_crawl'];
+    director = map['director'];
+    producer = map['producer'];
+    releaseDate = DateTime.parse(map['release_date']);
   }
 }
--- lib/SearchModule/SearchPageViews/SearchPageViews.dart
+++ lib/SearchModule/SearchPageViews/SearchPageViews.dart
@@ -1,259 +1,76 @@
-// Copyright (c) 2022 yichen <d.unicreators@gmail.com>. All rights reserved.
-// Use of this source code is governed by a MIT license that can be
-// found in the LICENSE file.
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_mvvm_example/view_models/main_page_view_model.dart';
+import 'package:flutter_mvvm_example/views/widgets/characters_panel.dart';
+import 'package:flutter_mvvm_example/views/widgets/films_panel.dart';
+import 'package:flutter_mvvm_example/views/widgets/planets_panel.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-part of './mvvm.dart';
+class SearchPageViews extends StatefulWidget {
+  final MainPageViewModel viewModel;
 
-/// View
-///
-abstract class View<TViewModel extends ViewModel> extends ViewWidget<TViewModel>
-    with ValueWidgetBuilderMixin {
-  /// View
-  const View({Key? key}) : super(key: key);
+  MainPage({Key key, @required this.viewModel}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
 }
 
-///
-/// ViewWidget
-///
-abstract class ViewWidget<TViewModel extends ViewModel> extends StatefulWidget {
-  /// ViewWidget
-  ///
-  const ViewWidget({Key? key}) : super(key: key);
+class SearchPageViews extends State<MainPage> with SingleTickerProviderStateMixin {
+  TabController tabController;
 
-  @protected
-  @override
-  @nonVirtual
-  State<ViewWidget> createState() =>
-      _ViewWidgetState<TViewModel>(createViewModel());
-
-  @override
-  @nonVirtual
-  StatefulElement createElement() => ViewElement<TViewModel>(this);
-
-  ///
-  @protected
-  @factory
-  TViewModel createViewModel();
-
-  ///
-  @protected
-  Widget build(ViewBuildContext<TViewModel> context, TViewModel model);
-
-  ///
-  @protected
-  @mustCallSuper
-  void didChangeDependencies(TViewModel model) {}
-
-  ///
-  @protected
-  @mustCallSuper
-  void activate(TViewModel model) {}
-
-  ///
-  @protected
-  @mustCallSuper
-  void deactivate(TViewModel model) {}
-
-  ///
-  @protected
-  @mustCallSuper
-  void didUpdateWidget(
-      covariant ViewWidget<TViewModel> oldWidget, TViewModel model) {}
-}
-
-/// ViewBuildContext
-///
-abstract class ViewBuildContext<TViewModel extends ViewModel>
-    implements
-        BuildContext,
-        BindableObject,
-        BindableObjectMixin,
-        BindableObjectValueMixin,
-        BindableObjectWidgetBuilderMixin {
-  /// setState
-  ///
-  void setState(VoidCallback fn);
-
-  /// ViewModel
-  ///
-  TViewModel get model;
-}
-
-/// ViewElement
-///
-class ViewElement<TViewModel extends ViewModel>
-    extends ViewElementBase<TViewModel>
-    with
-        BindableObjectMixin,
-        BindableObjectValueMixin,
-        BindableObjectWidgetBuilderMixin,
-        ValueWidgetBuilderMixin {
-  /// ViewElement
-  ViewElement(ViewWidget<TViewModel> widget) : super(widget);
-}
-
-/// ViewElementBase
-///
-abstract class ViewElementBase<TViewModel extends ViewModel>
-    extends StatefulElement implements ViewBuildContext<TViewModel> {
-  /// ViewElementBase
-  ///
-  ViewElementBase(ViewWidget widget) : super(widget);
-
-  @protected
-  @override
-  _ViewWidgetState<TViewModel> get state =>
-      super.state as _ViewWidgetState<TViewModel>;
-
-  /// ViewModel
-  ///
-  @override
-  TViewModel get model => state.model;
-
-  /// setState
-  ///
-  @override
-  void setState(VoidCallback fn) => state._setState(fn);
-
-  ///
-  /// 获取指定 [propertyKey] 对应的属性
-  ///
-  /// [propertyKey] 属性键
-  ///
-  /// [required] 指定 [propertyKey] 对应属性是否必须存在,
-  ///   其值为 `true` 时, 如 [propertyKey] 对应属性不存在则抛出异常
-  ///   默认值为 `false`
-  ///
-  @override
-  BindableProperty<TValue>? getProperty<TValue>(Object propertyKey,
-          {bool required = false}) =>
-      model.getProperty<TValue>(propertyKey, required: required);
-
-  ///
-  /// 注册一个绑定属性
-  ///
-  /// [propertyKey] 指定属性键
-  ///
-  /// [property] 指定绑定属性
-  ///
-  @override
-  BindableProperty<TValue> registerProperty<TValue>(
-          Object propertyKey, BindableProperty<TValue> property) =>
-      model.registerProperty(propertyKey, property);
-}
-
-class _ViewWidgetState<TViewModel extends ViewModel>
-    extends State<ViewWidget<TViewModel>> {
-  final TViewModel _model;
-  _ViewWidgetState(TViewModel model) : _model = model;
-
-  TViewModel get model => _model;
-
-  @override
-  Widget build(BuildContext context) =>
-      widget.build(context as ViewBuildContext<TViewModel>, model);
-
-  @override
-  void deactivate() {
-    widget.deactivate(model);
-    super.deactivate();
+  Future loadData() async {
+    await widget.viewModel.setFilms();
+    await widget.viewModel.setCharacters();
+    await widget.viewModel.setPlanets();
   }
-
-  @override
-  void activate() {
-    widget.activate(model);
-    super.activate();
-  }
-
-  @override
-  void didChangeDependencies() {
-    widget.didChangeDependencies(model);
-    super.didChangeDependencies();
-  }
-
-  @override
-  void didUpdateWidget(covariant ViewWidget<TViewModel> oldWidget) {
-    widget.didUpdateWidget(oldWidget, model);
-    super.didUpdateWidget(oldWidget);
-  }
-
-  void _setState(VoidCallback fn) => setState(fn);
-
-  @override
-  void initState() {
-    model.init();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    model.dispose();
-    super.dispose();
-  }
-}
-
-/// ValueListenableConditionalBuilder
-///
-class ValueListenableConditionalBuilder<T> extends ValueListenableBuilder<T> {
-  ///
-  final bool Function(T value) on;
-
-  /// ValueListenableConditionalBuilder
-  ///
-  const ValueListenableConditionalBuilder({
-    Key? key,
-    required ValueListenable<T> valueListenable,
-    required ValueWidgetBuilder<T> builder,
-    required this.on,
-    Widget? child,
-  }) : super(
-            key: key,
-            valueListenable: valueListenable,
-            builder: builder,
-            child: child);
-
-  @override
-  State<StatefulWidget> createState() =>
-      _ValueListenableConditionalBuilderState<T>();
-}
-
-class _ValueListenableConditionalBuilderState<T>
-    extends State<ValueListenableConditionalBuilder<T>> {
-  late T value;
 
   @override
   void initState() {
     super.initState();
-    value = widget.valueListenable.value;
-    widget.valueListenable.addListener(_valueChanged);
-  }
-
-  @override
-  void didUpdateWidget(ValueListenableConditionalBuilder<T> oldWidget) {
-    if (oldWidget.valueListenable != widget.valueListenable) {
-      oldWidget.valueListenable.removeListener(_valueChanged);
-      value = widget.valueListenable.value;
-      widget.valueListenable.addListener(_valueChanged);
-    }
-    super.didUpdateWidget(oldWidget);
-  }
-
-  @override
-  void dispose() {
-    widget.valueListenable.removeListener(_valueChanged);
-    super.dispose();
-  }
-
-  void _valueChanged() {
-    var value = widget.valueListenable.value;
-    if (widget.on(value))
-      setState(() {
-        this.value = value;
-      });
+    tabController = TabController(vsync: this, length: 3);
+    loadData();
   }
 
   @override
   Widget build(BuildContext context) {
-    return widget.builder(context, value, widget.child);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Star Wars',
+          style: TextStyle(
+            fontFamily: 'Distant Galaxy',
+          ),
+        ),
+        bottom: TabBar(
+          controller: tabController,
+          indicatorColor: Colors.white,
+          indicatorWeight: 3.0,
+          tabs: <Widget>[
+            Tab(icon: Icon(FontAwesomeIcons.film)),
+            Tab(icon: Icon(FontAwesomeIcons.users)),
+            Tab(icon: Icon(FontAwesomeIcons.globeAmericas))
+          ],
+        ),
+      ),
+      body: ScopedModel<MainPageViewModel>(
+        model: widget.viewModel,
+        child: TabBarView(
+          controller: tabController,
+          children: <Widget>[
+            FilmsPanel(),
+            CharactersPanel(),
+            PlanetsPanel(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    tabController?.dispose();
+    super.dispose();
   }
 }
